f372c3114921365a621da95d5ecc93c4e95bc602
Steve Antonio Silva <steveantoniosilva@gmail.com>
2022-06-07 08:03:46 -0400

    User Login, Password + Hash

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
 
 mongoose.connect('mongodb://localhost/my_database');
 
@@ -9,41 +8,34 @@ const ejs = require('ejs');
 
 app.set('view engine', 'ejs');
 
+const validateMiddleWare = require('./validationMiddleware/validateMiddleware');
+
+const fileUpload = require('express-fileupload');
+
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-const BlogPost = require('./models/BlogPost');
+app.use(fileUpload());
+app.use('/posts/store', validateMiddleWare);
 
 app.listen(1940, () => {
   console.log('diesel on port 1940');
 });
 
-app.get('/', async (req, res) => {
-  const blogposts = await BlogPost.find({})
-  console.log(blogposts);
-  res.render('index.ejs', {
-    blogposts
-  });
-});
+const homeController = require('./controllers/home');
+const getPostController = require('./controllers/getPost');
+const newPostController = require('./controllers/newPost');
+const storePostController = require('./controllers/storePost');
+const newUserController = require('./controllers/newUser');
+const storeUserController = require('./controllers/storeUser');
+const loginController = require('./controllers/login');
+const loginUserController = require('./controllers/loginUser');
 
-app.get('/about', (req, res) => {
-  res.render('about.ejs');
-});
-
-app.get('/contact', (req, res) => {
-  res.render('contact.ejs');
-});
-
-app.get('/post', (req, res) => {
-  res.render('post.ejs');
-});
-
-app.get('/posts/new', (req, res) => {
-  res.render('create.ejs');
-});
-
-app.post('/posts/store', async (req, res) => {
-  await BlogPost.create(req.body);
-  res.redirect('/');
-});
+app.get('/', homeController);
+app.get('/post/:id', getPostController);
+app.get('/posts/new', newPostController);
+app.post('/posts/store', storePostController);
+app.get('/auth/register', newUserController);
+app.post('/users/register', storeUserController);
+app.get('/auth/login', loginController);
+app.post('/users/login', loginUserController);
--- models/BlogPost.js
+++ models/BlogPost.js
@@ -6,6 +6,12 @@ const BlogPostSchema = new Schema(
   {
     title: String,
     body: String,
+    username: String,
+    datePosted: {
+      type: Date,
+      default: new Date(),
+    },
+    image: String,
   },
   {
     timestamps: true,
@@ -13,5 +19,4 @@ const BlogPostSchema = new Schema(
 );
 
 const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
-
 module.exports = BlogPost;
