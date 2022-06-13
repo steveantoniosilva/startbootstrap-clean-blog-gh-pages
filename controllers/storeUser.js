a1884616b9e0c64ee86e8f9f80600bd02ab67720
Steve Antonio Silva <steveantoniosilva@gmail.com>
2022-06-13 15:27:39 -0400

    404 not found

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -4,6 +4,7 @@ const path = require('path');
 module.exports = (req, res) => {
   User.create(req.body, (error, user) => {
     if (error) {
+      console.log('error', error);
       return res.redirect('/auth/register');
     }
     res.redirect('/');
