6969e389d178e8c17780842745a610877edc02c4
Steve Antonio Silva <steveantoniosilva@gmail.com>
2022-06-19 11:31:54 -0400

    debugging heroku, removing .env connection MongoDB

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
+// require('dotenv').config();
+
+// mongoose.connect(
+//   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lhdg4.mongodb.net/BlogPostDatabase`,
+//   () => {
+//     console.log('MongoDB has taken flight...');
+//   }
+// );
 
 mongoose.connect(
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lhdg4.mongodb.net/BlogPostDatabase`,
+  `mongodb+srv://stevesilva:MongoDBMongoose@cluster0.lhdg4.mongodb.net/BlogPostDatabase`,
   () => {
     console.log('MongoDB has taken flight...');
   }
