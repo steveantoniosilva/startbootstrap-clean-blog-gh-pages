71595fa8a48da431257a41580233ae460d7c8248
Steve Antonio Silva <steveantoniosilva@gmail.com>
2022-06-19 13:07:01 -0400

    removed .populate from home.js

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,8 @@
 const BlogPost = require('../models/BlogPost');
 
 module.exports = async (req, res) => {
-  const blogposts = await BlogPost.find({}).populate('userid');
+  const blogposts = await BlogPost.find({})
+  // .populate('userid');
   res.render('index', {
     blogposts,
   });
